b504d598cb9fdc9bd375a29c04e6643939311b80
Elliot Woods <elliot.woods@gmail.com>
2013-08-28 18:31:17 +0900

    Ximea DX11 updates

---
+++ src/nodes/plugins/DX11/Ximea/Lib/HardwareTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VVVV.Nodes.Ximea
+{
+	class HardwareTrigger : ITrigger
+	{
+		public enum HardwareEvent
+		{
+			RisingEdge,
+			FallingEdge,
+			WhilstHigh
+		}
+
+		public HardwareEvent Source;
+
+		public TriggerType GetTriggerType()
+		{
+			return TriggerType.GPI;
+		}
+	}
+}
--- src/nodes/plugins/DX11/Ximea/Nodes/VideoInDX11.cs
+++ src/nodes/plugins/DX11/Ximea/Nodes/VideoInDX11.cs
@@ -24,12 +24,12 @@ namespace VVVV.Nodes.Ximea
 				Help = "Capture from Ximea camera to DX11 texture",
 				Tags = "")]
 	#endregion PluginInfo
-	public class VideoInDX11Node : IPluginEvaluate, IDisposable, IDX11ResourceProvider
+	public class VideoInDX11Node : IPluginEvaluate, IDisposable, IDX11ResourceProvider, IPartImportsSatisfiedNotification
 	{
 		#region fields & pins
 		IPluginHost FHost;
 
-		[Input("Device ID")]
+		[Input("Device ID", IsSingle=true)]
 		ISpread<int> FInDeviceID;
 
 		[Input("Timeout", MinValue = 0, DefaultValue=500)]
@@ -62,6 +62,12 @@ namespace VVVV.Nodes.Ximea
 		[Output("Specification")]
 		ISpread<Device.Specification> FOutSpecification;
 
+		[Output("Status")]
+		ISpread<string> FOutStatus;
+
+//		[Import]
+//		IHDEHost FHDEHost;
+
 		Device FDevice = new Device();
 		#endregion fields & pins
 
@@ -80,49 +86,46 @@ namespace VVVV.Nodes.Ximea
 
 			bool MaybeReinitialised = false;
 
-			if (this.FInTrigger.IsChanged)
+			try
 			{
-				this.FDevice.Trigger = this.FInTrigger[0];
-				MaybeReinitialised = true;
-			}
-
-			if (this.FInDeviceID.IsChanged)
-			{
-				if (this.FDevice.DeviceID != this.FInDeviceID[0])
+				if (this.FInTrigger.IsChanged)
 				{
-					this.FDevice.DeviceID = this.FInDeviceID[0];
+					this.FDevice.Trigger = this.FInTrigger[0];
 					MaybeReinitialised = true;
 				}
-			}
 
-			if (this.FInEnabled.IsChanged)
-			{
-				this.FDevice.Enabled = this.FInEnabled[0];
-				MaybeReinitialised = true;
-			}
-
-			if (MaybeReinitialised)
-			{
-				FOutSpecification[0] = FDevice.DeviceSpecification;
-			}
-
-			if ((FInParameterSet.IsChanged || MaybeReinitialised) && FInParameterSet[0] != null)
-			{
-				foreach (var parameter in FInParameterSet[0])
+				if (this.FInDeviceID.IsChanged)
 				{
-					FDevice.SetParameter(parameter.Key, parameter.Value);
+					if (this.FDevice.DeviceID != this.FInDeviceID[0])
+					{
+						this.FDevice.DeviceID = this.FInDeviceID[0];
+						MaybeReinitialised = true;
+					}
+				}
+
+				if (this.FInEnabled.IsChanged)
+				{
+					this.FDevice.Enabled = this.FInEnabled[0];
+					MaybeReinitialised = true;
+				}
+
+				if ((FInParameterSet.IsChanged || MaybeReinitialised) && FInParameterSet[0] != null)
+				{
+					foreach (var parameter in FInParameterSet[0])
+					{
+						FDevice.SetParameter(parameter.Key, parameter.Value);
+					}
+				}
+
+				if (MaybeReinitialised)
+				{
+					FOutSpecification[0] = FDevice.DeviceSpecification;
+					FOutStatus[0] = "OK";
 				}
 			}
-
-			if (FInWaitForFrame[0] && FDevice.Running)
+			catch(Exception e)
 			{
-				Stopwatch Timer = new Stopwatch();
-				Timer.Start();
-
-				while (Timer.ElapsedMilliseconds < FInTimeout[0] && !FDevice.DataNew)
-				{
-					Thread.Sleep(1);
-				}
+				FOutStatus[0] = e.Message;
 			}
 
 			FOutRunning[0] = FDevice.Running;
@@ -154,6 +157,33 @@ namespace VVVV.Nodes.Ximea
 			}
 
 			FDevice.Dispose();
+		}
+
+		public void OnImportsSatisfied()
+		{
+			//FHDEHost.MainLoop.OnPresent += MainLoop_OnPresent;
+		}
+
+		void MainLoop_OnPresent(object sender, EventArgs e)
+		{
+			if (FInWaitForFrame[0] && FDevice.Running)
+			{
+				TimeSpan sleepTime = new TimeSpan(100);
+				Stopwatch waitingTime = new Stopwatch();
+				waitingTime.Start();
+
+				int timeout = FInTimeout[0];
+				FDevice.UpdateFrameAvailable();
+
+				while (!FDevice.DataNew)
+				{
+					Thread.Sleep(sleepTime);
+					FDevice.UpdateFrameAvailable();
+
+					if (waitingTime.ElapsedMilliseconds > timeout)
+						break;
+				}
+			}
 		}
 	}
 }
