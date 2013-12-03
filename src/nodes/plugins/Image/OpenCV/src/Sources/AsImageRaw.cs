15bfe11a0c2dcd1af5e90faed94bd76eda3839d7
joreg <joreg@vvvv.org>
2013-12-03 20:32:29 +0100

    moved AsImage nodes to correct category

--- src/nodes/plugins/Image/OpenCV/src/Sources/AsImageRaw.cs
+++ src/nodes/plugins/Image/OpenCV/src/Sources/AsImageRaw.cs
@@ -107,7 +107,7 @@ namespace VVVV.CV.Nodes
 	}
 
 	#region PluginInfo
-	[PluginInfo(Name = "AsImage", Category = "CV.Image", Version = "Raw", Help = "Loads Raw stream into image", Tags = "source")]
+	[PluginInfo(Name = "AsImage", Category = "Raw", Version = "", Help = "Loads Raw stream into image", Tags = "cv, source")]
 	#endregion PluginInfo
 	public class AsImageRawNode : IGeneratorNode<AsImageRawInstance>
 	{
