03220adedcd4f02a8a3acccac1fc26a8cc7a19a6
Benjamin Denoziere <benjamin.denoziere@phileogeleven.com>
2013-12-04 20:59:55 +0100

    Edit SolvePnP to make it look more like CalibrateCamera, with wrapper patch, based on CalibrateProjector Add new IntrinsicsJoin node

---
+++ src/nodes/plugins/Image/OpenCV/src/Calibration/IntrinsicsJoinNode.cs
@@ -0,0 +1,87 @@
+﻿#region usings
+using System;
+using System.ComponentModel.Composition;
+using System.Drawing;
+using System.Threading;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+using VVVV.Core.Logging;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using ThreadState = System.Threading.ThreadState;
+using System.Collections.Generic;
+
+#endregion usings
+
+namespace VVVV.CV.Nodes
+{
+
+	#region PluginInfo
+	[PluginInfo(Name = "Intrinsics", Category = "CV.Transform", Version = "Join", Help = "Join intrinsics params", Tags = "")]
+	#endregion PluginInfo
+	public class IntrinsicsJoinNode : IPluginEvaluate, IDisposable
+	{
+		#region fields & pins
+
+        [Input("Sensor Size")]
+        ISpread<Vector2D> FPinInSensorSize;
+
+        [Input("VVVV Projection Transform")]
+        ISpread<Matrix4x4> FPinInCameraTransform;
+
+        [Input("Distortion Coefficients")]
+        ISpread<ISpread<Double>> FPinInDistiortonCoefficients;
+
+		[Output("Intrinsics")]
+        ISpread<Intrinsics> FPinOutIntrinsics;
+
+
+
+		[Import]
+		ILogger FLogger;
+
+		#endregion fields & pins
+
+		[ImportingConstructor]
+        public IntrinsicsJoinNode(IPluginHost host)
+		{
+
+		}
+
+		public void Dispose()
+		{
+		}
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			//if (!FPinInIntrinsics.IsChanged)
+			//    return;
+
+            if (FPinInCameraTransform[0] == null)
+			{
+                FPinOutIntrinsics.SliceCount = 0;
+			}
+			else
+			{
+                FPinOutIntrinsics.SliceCount = SpreadMax;
+
+				for (int i = 0; i < SpreadMax; i++)
+				{
+                    IntrinsicCameraParameters cp = new IntrinsicCameraParameters();
+                    // Inspiration / details from http://strawlab.org/2011/11/05/augmented-reality-with-OpenGL/ - See Intrinsics.cs too
+                    cp.IntrinsicMatrix[0, 0] = FPinInCameraTransform[i][0, 0] * FPinInSensorSize[i].x / 2;// =L15*P8/2
+                    cp.IntrinsicMatrix[1, 1] = FPinInCameraTransform[i][1, 1] * FPinInSensorSize[i].y / 2;// =L15*P8/2
+                    cp.IntrinsicMatrix[0, 2] = (FPinInCameraTransform[i][2, 0] + 1)* FPinInSensorSize[i].x / 2;// =L15*P8/2
+                    cp.IntrinsicMatrix[1, 2] = (FPinInCameraTransform[i][2, 1] +1) * FPinInSensorSize[i].y / 2;// =L15*P8/2
+                    cp.IntrinsicMatrix[2, 2] = 1;
+                    FPinOutIntrinsics[i] = new Intrinsics(cp, new Size((int)FPinInSensorSize[i].x, (int)FPinInSensorSize[i].y));
+				}
+			}
+		}
+	}
+}
--- src/nodes/plugins/Image/OpenCV/src/Calibration/SolvePnP.cs
+++ src/nodes/plugins/Image/OpenCV/src/Calibration/SolvePnP.cs
@@ -20,6 +20,8 @@ using VVVV.CV.Core;
 
 namespace VVVV.CV.Nodes
 {
+	public enum TCoordinateSystem { VVVV, OpenCV };
+
 	#region PluginInfo
 	[PluginInfo(Name = "SolvePnP", Category = "CV.Transform", Help = "Find extrinsics of object given camera intrinsics and some image<>object correspondences", Tags = "FindExtrinsics")]
 	#endregion PluginInfo
@@ -27,7 +29,7 @@ namespace VVVV.CV.Nodes
 	{
 		#region fields & pins
 		[Input("Object Points")]
-		ISpread<ISpread<Vector3D> > FPinInObject;
+		ISpread<ISpread<Vector3D>> FPinInObject;
 
 		[Input("Image Points")]
 		ISpread<ISpread<Vector2D>> FPinInImage;
@@ -35,8 +37,17 @@ namespace VVVV.CV.Nodes
 		[Input("Intrinsics")]
 		ISpread<Intrinsics> FPinInIntrinsics;
 
+		[Input("Coordinates", IsSingle = true)]
+		ISpread<TCoordinateSystem> FPinInCoordSystem;
+
+		[Input("Do", IsBang = true, IsSingle = true)]
+		ISpread<bool> FPinInDo;
+
 		[Output("Extrinsics")]
 		ISpread<Extrinsics> FPinOutExtrinsics;
+
+		[Output("View per board")]
+		ISpread<Matrix4x4> FPinOutView;
 
 		[Output("Status")]
 		ISpread<String> FPinOutStatus;
@@ -49,7 +60,6 @@ namespace VVVV.CV.Nodes
 		[ImportingConstructor]
 		public SolvePnPNode(IPluginHost host)
 		{
-
 		}
 
 		public void Dispose()
@@ -60,33 +70,43 @@ namespace VVVV.CV.Nodes
 		//called when data for any output pin is requested
 		public void Evaluate(int SpreadMax)
 		{
-			SpreadMax = Math.Max(FPinInObject.SliceCount, FPinInImage.SliceCount);
-
-			FPinOutExtrinsics.SliceCount = SpreadMax;
-			FPinOutStatus.SliceCount = SpreadMax;
-
-			for (int i = 0; i < SpreadMax; i++)
+			if (FPinInDo[0])
 			{
-				try
-				{
-					if (FPinInObject[i].SliceCount == 0 || FPinInImage[i].SliceCount == 0)
-						throw new Exception("No datapoints");
-					if (FPinInImage[i].SliceCount == 1)
-						throw new Exception("Only 1 image point is being input per board, check SliceCount!");
-					if (FPinInObject[i].SliceCount == 1)
-						throw new Exception("Only 1 object point is being input per board, check SliceCount!");
-					if (FPinInIntrinsics[i].intrinsics == null)
-						throw new Exception("Waiting for camera calibration intrinsics");
+				bool useVVVVCoords = FPinInCoordSystem[0] == TCoordinateSystem.VVVV;
 
-					ExtrinsicCameraParameters extrinsics = CameraCalibration.FindExtrinsicCameraParams2(MatrixUtils.ObjectPoints(FPinInObject[i], true), MatrixUtils.ImagePoints(FPinInImage[i]), FPinInIntrinsics[i].intrinsics);
-					FPinOutExtrinsics[i] = new Extrinsics(extrinsics);
-					FPinOutStatus[i] = "OK";
-				}
-				catch (Exception e)
+				SpreadMax = Math.Max(FPinInObject.SliceCount, FPinInImage.SliceCount);
+
+				FPinOutExtrinsics.SliceCount = SpreadMax;
+				FPinOutStatus.SliceCount = SpreadMax;
+
+				for (int i = 0; i < SpreadMax; i++)
 				{
-					FPinOutStatus[i] = e.Message;
+					try
+					{
+						if (FPinInObject[i].SliceCount == 0 || FPinInImage[i].SliceCount == 0)
+							throw new Exception("No datapoints");
+						if (FPinInImage[i].SliceCount == 1)
+							throw new Exception("Only 1 image point is being input per board, check SliceCount!");
+						if (FPinInObject[i].SliceCount == 1)
+							throw new Exception("Only 1 object point is being input per board, check SliceCount!");
+						if (FPinInIntrinsics[i].intrinsics == null)
+							throw new Exception("Waiting for camera calibration intrinsics");
+
+						ExtrinsicCameraParameters extrinsics = CameraCalibration.FindExtrinsicCameraParams2(MatrixUtils.ObjectPoints(FPinInObject[i], useVVVVCoords), MatrixUtils.ImagePoints(FPinInImage[i]), FPinInIntrinsics[i].intrinsics);
+						FPinOutExtrinsics[i] = new Extrinsics(extrinsics);
+
+						if (useVVVVCoords)
+							FPinOutView[i] = MatrixUtils.ConvertToVVVV(FPinOutExtrinsics[i].Matrix);
+						else
+							FPinOutView[i] = FPinOutExtrinsics[i].Matrix;
+
+						FPinOutStatus[i] = "OK";
+					}
+					catch (Exception e)
+					{
+						FPinOutStatus[i] = e.Message;
+					}
 				}
-				
 			}
 		}
 	}
