6110ba6dcef72332b1882b0d6032c4ab0086012e
smakhtin <algariol@gmail.com>
2014-04-16 15:32:08 +0400

    Added help file, fixed path original.

--- src/nodes/plugins/Image/OpenCV/src/Filters/WithinRangeHSV.cs
+++ src/nodes/plugins/Image/OpenCV/src/Filters/WithinRangeHSV.cs
@@ -7,18 +7,30 @@ using VVVV.CV.Core;
 
 namespace VVVV.CV.Nodes
 {
-    [FilterInstance("WithinRange", Version = "HSV", Author = "alg", Help = "Check if value is in target HSV range")]
+    [FilterInstance("WithinRangeHSV", Version = "HSV", Author = "alg", Help = "Check if value is in target HSV range")]
     public class WithinRangeHsvInstance : IFilterInstance
     {
-        [Input("Minimum", DefaultValues = new double[] {0, 0, 0}, MinValue = 0, MaxValue = 1)] public Vector3D Minimum;
+        [Input("Minimum", DefaultValues = new double[] {0, 0, 0}, MinValue = 0, MaxValue = 1)] 
+        public Vector3D Minimum;
 
-        [Input("Maximum", DefaultValues = new double[] {1, 1, 1}, MinValue = 0, MaxValue = 1)] public Vector3D Maximum;
+        [Input("Maximum", DefaultValues = new double[] {1, 1, 1}, MinValue = 0, MaxValue = 1)] 
+        public Vector3D Maximum;
+
+        [Input("Pass Original", DefaultBoolean = false, IsToggle = true, IsSingle = true)] 
+        public bool PassOriginal;
+
+        private readonly CVImage FHsvImage = new CVImage();
+        private readonly CVImage FBuffer = new CVImage();
+
 
         private double FMult = byte.MaxValue;
 
         public override void Allocate()
         {
-            FOutput.Image.Initialise(FInput.Image.ImageAttributes.Size, TColorFormat.L8);
+            FHsvImage.Initialise(FInput.ImageAttributes.Size, TColorFormat.HSV32F);
+            FBuffer.Initialise(FInput.ImageAttributes.Size, TColorFormat.L8);
+            
+            FOutput.Image.Initialise(FInput.Image.ImageAttributes);
 
             FMult = FInput.ImageAttributes.BytesPerPixel > 4 ? float.MaxValue : byte.MaxValue;
         }
@@ -27,11 +39,26 @@ namespace VVVV.CV.Nodes
         {
             if (!FInput.LockForReading()) return;
 
-            CvInvoke.cvInRangeS(FInput.CvMat, new MCvScalar(Minimum.x*FMult, Minimum.y*FMult, Minimum.z*FMult),
-                new MCvScalar(Maximum.x*FMult, Maximum.y*FMult, Maximum.z*FMult), FOutput.CvMat);
+            FInput.GetImage(FHsvImage);
+            
             FInput.ReleaseForReading();
 
-            FOutput.Send();
+            CvInvoke.cvInRangeS(FHsvImage.CvMat, new MCvScalar(Minimum.x * FMult, Minimum.y * FMult, Minimum.z * FMult),
+                    new MCvScalar(Maximum.x * FMult, Maximum.y * FMult, Maximum.z * FMult), FBuffer.CvMat);
+
+            if (PassOriginal)
+            {
+                FOutput.Image.SetImage(FInput.Image);
+
+                CvInvoke.cvNot(FBuffer.CvMat, FBuffer.CvMat);
+                CvInvoke.cvSet(FOutput.Image.CvMat, new MCvScalar(0.0), FBuffer.CvMat);
+
+                FOutput.Send();
+            }
+            else
+            {
+                FOutput.Send(FBuffer);
+            }
         }
     }
 }
